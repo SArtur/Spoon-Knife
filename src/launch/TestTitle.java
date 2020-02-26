575fa421fe0ada5897e628cd706817af202f7a89
SArtur <artewal@gmail.com>
2020-02-26 11:49:07 +0100

    Created new method which is `String concateStrings` ```javascript public String concateStrings(String s1, String s2) { return s1 + s2;} } ```` Col1 | Col 2 --|-- row1 | row2
    Checked list
    -[] Not done
    -[x] done

--- src/launch/TestTitle.java
+++ src/launch/TestTitle.java
@@ -4,5 +4,9 @@ public class TestTitle {
 	public void testingMethod() {
 		
 	}
+public static void main(String[] args) {
+	System.out.println("blah blah");
+}
 
+public String concateStrings(String s1, String s2) { return s1 + s2;}
 }
